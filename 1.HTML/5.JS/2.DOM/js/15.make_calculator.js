cfad48aa041388a6ae968cd1a64677c5c1b00660
namgijeong <dreaming1817@gmail.com>
2025-11-20 21:40:55 +0900

    [과제] 계산기 수식 예외처리 -맨앞에 연산기호가 있는지 검사 -마지막이 숫자로 끝나는지 검사 -에러 문구인 상태로 또 눌렀는지 검사

--- 1.HTML/5.JS/2.DOM/js/15.make_calculator.js
+++ 1.HTML/5.JS/2.DOM/js/15.make_calculator.js
@@ -12,19 +12,43 @@ buttonGridItems.forEach((button) => {
 });
 
 function clickButton(e){
-    console.log(e.target.textContent);
+    //console.log(e.target.textContent);
+    let currentInput = e.target.textContent;
     
-    if (e.target.textContent == "="){
-        let result = eval(inputString);
-        console.log(result);
-        //이때 계산했던 결과면 숫자이므로 나중에 C를 눌러 지울때 slice가 적용안됨
-        inputString = ''+result;
-    }else if (e.target.textContent == "C"){
-        let length = inputString.length;
-      
-        inputString = inputString.slice(0,length-1); 
+    //만약 아무숫자도 입력안했는데 연산기호가 먼저 입력되면 안된다 => syntax error
+    //아무것도 입력안하고 = 를 누르면 undefined 결과값
+    //연산기호가 중간에 왔는데 끝이 숫자로 끝나지 않으면 에러 => syntax error
+    //나누기 기호 뒤에 0이 오면 안된다 => Infinity로 결과값
+
+    if (currentInput == "="){
+        //맨앞에 연산기호가 있는지 검사
+        if (inputString[0] == '+' || inputString[0] == '-' || inputString[0] == '*' || inputString[0] == '/'){
+            inputString = 'error: 유효한 식이 아닙니다.';
+        }
+
+        //마지막이 숫자로 끝나는지 검사
+        if (inputString[inputString.length-1] != '1' && inputString[inputString.length-1] != '2' && inputString[inputString.length-1] != '3' && inputString[inputString.length-1] != '4' && inputString[inputString.length-1] != '5' && inputString[inputString.length-1] != '6' && inputString[inputString.length-1] != '7' && inputString[inputString.length-1] != '8' && inputString[inputString.length-1] != '9'){
+            inputString = 'error: 유효한 식이 아닙니다.';
+        }
+        //9 ****1 같은 경우는 소용이 없는데? = > 기호를 입력할시 앞에 이미 입력한 기호가 있는지 검사
+        //혹은 기호가 나타나면 기호 바로 뒤에 숫자 있는지 확인
+        //리턴값이 undefined, Infinity인지 검사
+
+        //에러문구인 상태로 또 누르면 
+        else if (inputString.includes('error')){
+            inputString = 'error: 유효한 식이 아닙니다.';
+        }
+        else{
+            let result = eval(inputString);
+            console.log(result);
+            //이때 계산했던 결과면 숫자이므로 나중에 C를 눌러 지울때 slice가 적용안됨
+            inputString = ''+result;
+        }
+        
+    }else if (currentInput == "C"){
+        inputString = inputString.slice(0,inputString.length-1); 
     }else {
-        inputString += e.target.textContent;
+        inputString += currentInput;
     }
 
     calWindow.textContent = inputString;
