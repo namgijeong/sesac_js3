e88b967f4b84c406e72fbbd65ad8a0fdf1c9fa6d
namgijeong <dreaming1817@gmail.com>
2025-11-24 16:42:26 +0900

    [학습] 프로그래스 바 구현 -랜덤게임 주석 추가 및 setInterval로 자동입력 추가

--- 1.HTML/5.JS/2.DOM/js/14.guess_the_number_game.js
+++ 1.HTML/5.JS/2.DOM/js/14.guess_the_number_game.js
@@ -1,27 +1,39 @@
 let correctAnswer = 1;
-// let playCount = 0;
 
 //페이지에 진입하자마자 정답을 생성한다.
 makeRandomNumber();
 
-function makeRandomNumber(){
-    correctAnswer = Math.floor(Math.random() * 100 + 1);
+autoPlay();
+
+
+function makeRandomNumber() {
+    //Math.random() => 0. 0 ~ 9.99
+    //두자리수면 100을 곱하면 되겠다
+    //2.87 => 어떻게 달까?
+    //내림 => 0~99 => floor
+    //올림 => 0~100 =>round
+    //올림 => 1~100 ceil
+    correctAnswer = Math.floor(Math.random() * 100) + 1;
 }
 
-function checkAnswer(guess){
+function checkAnswer(guess) {
+    let trialButton = document.getElementById("trial");
+
     let feedbackString = '';
 
-    if (guess == ''){
+    if (guess == '') {
         feedbackString = "Please Enter the number.";
         return feedbackString;
     }
 
-    if (guess > correctAnswer){
+    if (guess > correctAnswer) {
         feedbackString = 'Too high!';
-    } else if (guess < correctAnswer){
+    } else if (guess < correctAnswer) {
         feedbackString = 'Too low!';
     } else {
         feedbackString = 'Correct! You guessed the number!';
+        trialButton.disabled = true;
+        document.getElementById("guess").disabled = true
     }
 
     return feedbackString;
@@ -29,20 +41,86 @@ function checkAnswer(guess){
 
 
 
-function playGame(){
+function playGame() {
     let resultDiv = document.getElementById("feed_back");
     let historyUl = document.getElementById("history");
     let guess = document.getElementById("guess").value;
+    let feedbackString;
 
-    //playCount++;
+    feedbackString = checkAnswer(guess);
 
-    let feedbackString = checkAnswer(guess);
-
-    let totalFeedbackString = '<b>'+feedbackString+'</b> <br/>';
+    let totalFeedbackString = '<b>' + feedbackString + '</b> <br/>';
     resultDiv.innerHTML = totalFeedbackString;
 
     let historyString = `You guessed ${guess}: ${feedbackString} <br/>`;
     let li = document.createElement('li');
     li.innerHTML = historyString;
     historyUl.appendChild(li);
+}
+
+function autoPlay() {
+    let resultDiv = document.getElementById("feed_back");
+    let historyUl = document.getElementById("history");
+    const guesses = [10, 20, 50, 70, 80, 60, 30, 40]
+
+    let feedbackString;
+    // for (guess of guesses) {
+    //     document.getElementById("guess").value = guess;
+    //     feedbackString = checkAnswer(guess);
+
+    //     let totalFeedbackString = '<b>' + feedbackString + '</b> <br/>';
+    //     resultDiv.innerHTML = totalFeedbackString;
+
+    //     let historyString = `You guessed ${guess}: ${feedbackString} <br/>`;
+    //     let li = document.createElement('li');
+    //     li.innerHTML = historyString;
+    //     historyUl.appendChild(li);
+    // }
+
+
+    // for (let guess = 1; guess <= 100; guess++) {
+    //     document.getElementById("guess").value = guess;
+    //     feedbackString = checkAnswer(guess);
+
+    //     console.log("입력시도: ", guess);
+
+    //     let totalFeedbackString = '<b>' + feedbackString + '</b> <br/>';
+    //     resultDiv.innerHTML = totalFeedbackString;
+
+    //     let historyString = `You guessed ${guess}: ${feedbackString} <br/>`;
+    //     let li = document.createElement('li');
+    //     li.innerHTML = historyString;
+    //     historyUl.appendChild(li);
+
+    //     if (feedbackString.includes("Correct")) {
+    //         break;
+    //     }
+    // }
+
+
+    const timerId = setInterval(
+        () => {
+            for (let guess = 1; guess <= 100; guess++) {
+                document.getElementById("guess").value = guess;
+                feedbackString = checkAnswer(guess);
+
+                console.log("입력시도: ", guess);
+
+                let totalFeedbackString = '<b>' + feedbackString + '</b> <br/>';
+                resultDiv.innerHTML = totalFeedbackString;
+
+                let historyString = `You guessed ${guess}: ${feedbackString} <br/>`;
+                let li = document.createElement('li');
+                li.innerHTML = historyString;
+                historyUl.appendChild(li);
+
+                if (feedbackString.includes("Correct")) {
+                    clearInterval(timerId);
+                    break;
+                }
+            }
+        }
+        , 1000);
+
+
 }
