c441b2495db4f9f95eaad1e429a1f6bf90614f46
namgijeong <dreaming1817@gmail.com>
2025-11-28 12:44:38 +0900

    [과제] stop watch timer -UI 구현

---
+++ 1.HTML/5.JS/4.MINIAPPS/10.stop_watch_timer.js
@@ -0,0 +1,9 @@
+const startButton = document.getElementById('start_button');
+const stopButton = document.getElementById('stop_button');
+const resetButton = document.getElementById('reset_button');
+
+let timerInterval;
+
+startButton.addEventListener('click',() => {
+
+});
