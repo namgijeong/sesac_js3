5f1ac0509ea7c6e62e5f6bbb066215b3cef9f8d5
namgijeong <dreaming1817@gmail.com>
2025-11-28 15:19:31 +0900

    [과제] stop watch - 시작, 스탑, 리셋 버튼 기능 구현 -숫자 ui 크게 개선

--- 1.HTML/5.JS/4.MINIAPPS/10.stop_watch_timer.js
+++ 1.HTML/5.JS/4.MINIAPPS/10.stop_watch_timer.js
@@ -2,8 +2,43 @@ const startButton = document.getElementById('start_button');
 const stopButton = document.getElementById('stop_button');
 const resetButton = document.getElementById('reset_button');
 
+const minutesText = document.getElementById('minute_text');
+const secondsText = document.getElementById('seconds_text');
+const msText = document.getElementById('ms_text');
+
 let timerInterval;
 
-startButton.addEventListener('click',() => {
+let timeCount = 0;
+let minutes = 0;
+let seconds = 0;
+let ms = 0;
 
+startButton.addEventListener('click',() => {
+    timerInterval = setInterval(() => {
+        timeCount += 1;
+        //밀리세컨즈 계산
+        ms = Math.floor(timeCount / 10) % 100;
+        //초계산
+        seconds = Math.floor(timeCount / 1000) % 60;
+        //분계산
+        minutes = Math.floor(timeCount / (1000* 60));
+
+        msText.textContent = ms.toString().padStart(2,'0');
+        secondsText.textContent = seconds.toString().padStart(2,'0');
+        minutesText.textContent = minutes.toString().padStart(2,'0');
+    },1);
 });
+
+stopButton.addEventListener('click', () => {
+    console.log('stop 눌림');
+    clearInterval(timerInterval);
+});
+
+resetButton.addEventListener('click', () => {
+    stopButton.click();
+
+    timeCount = 0;
+    msText.textContent = '00';
+    secondsText.textContent = '00';
+    minutesText.textContent = '00';
+})
