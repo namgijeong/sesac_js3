97a9d13b60b01f7afa5d7bcbe8c314fa940d47ae
namgijeong <dreaming1817@gmail.com>
2025-11-28 16:04:47 +0900

    [과제] stop watch -start 버튼 누르고 또 start 버튼 동작하지 않도록 -start를 눌러 진행중인 상황이 아닐때, reset, stop 버튼이 동작하지 않도록

--- 1.HTML/5.JS/4.MINIAPPS/10.stop_watch_timer.js
+++ 1.HTML/5.JS/4.MINIAPPS/10.stop_watch_timer.js
@@ -13,32 +13,69 @@ let minutes = 0;
 let seconds = 0;
 let ms = 0;
 
-startButton.addEventListener('click',() => {
-    timerInterval = setInterval(() => {
-        timeCount += 1;
-        //밀리세컨즈 계산
-        ms = Math.floor(timeCount / 10) % 100;
-        //초계산
-        seconds = Math.floor(timeCount / 1000) % 60;
-        //분계산
-        minutes = Math.floor(timeCount / (1000* 60));
+startButton.addEventListener('click', () => {
+    if (timerInterval) {
+        return;
+    }
+    else {
+        console.log("start");
+        startButton.style.backgroundColor = '#daeef0';
+        stopButton.style.backgroundColor = 'lightblue';
+        resetButton.style.backgroundColor = 'lightblue';
 
-        msText.textContent = ms.toString().padStart(2,'0');
-        secondsText.textContent = seconds.toString().padStart(2,'0');
-        minutesText.textContent = minutes.toString().padStart(2,'0');
-    },1);
+        timerInterval = setInterval(() => {
+            timeCount += 1;
+            //밀리세컨즈 계산
+            ms = Math.floor(timeCount / 10) % 100;
+            //초계산
+            seconds = Math.floor(timeCount / 1000) % 60;
+            //분계산
+            minutes = Math.floor(timeCount / (1000 * 60));
+
+            msText.textContent = ms.toString().padStart(2, '0');
+            secondsText.textContent = seconds.toString().padStart(2, '0');
+            minutesText.textContent = minutes.toString().padStart(2, '0');
+        }, 1);
+    }
+
 });
 
 stopButton.addEventListener('click', () => {
-    console.log('stop 눌림');
-    clearInterval(timerInterval);
+    if (!timerInterval) {
+        return;
+    }
+    else {
+        console.log("stop");
+        startButton.style.backgroundColor = 'lightblue';
+        stopButton.style.backgroundColor = '#daeef0';
+        resetButton.style.backgroundColor = 'lightblue';
+
+        clearInterval(timerInterval);
+        //여기서 알수있는건 clearInterval시 timerInterval 변수값이 사라지는건 아니다
+        console.log('stop시 timeinterval:', timerInterval);
+        timerInterval = undefined;
+    }
 });
 
 resetButton.addEventListener('click', () => {
-    stopButton.click();
+    startButton.style.backgroundColor = 'lightblue';
+    stopButton.style.backgroundColor = 'lightblue';
+    resetButton.style.backgroundColor = '#daeef0';
 
     timeCount = 0;
     msText.textContent = '00';
     secondsText.textContent = '00';
     minutesText.textContent = '00';
-})
+
+    if (!timerInterval) {
+        return;
+    }
+    else {
+        console.log("reset");
+
+        clearInterval(timerInterval);
+        //여기서 알수있는건 clearInterval시 timerInterval 변수값이 사라지는건 아니다
+        timerInterval = undefined;
+    }
+
+});
