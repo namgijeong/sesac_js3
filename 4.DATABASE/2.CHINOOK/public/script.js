81f6397b1d8c40df91f4c3038ad3b5832e0fbdff
namgijeong <dreaming1817@gmail.com>
2025-12-21 16:49:32 +0900

    [과제] chinook 장르별 트랙 검색 구현

--- 4.DATABASE/2.CHINOOK/public/script.js
+++ 4.DATABASE/2.CHINOOK/public/script.js
@@ -1,4 +1,43 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    fetch
+const genreSelectTag = document.getElementById("genre_select");
+const genreSearchButton = document.getElementById("genre_search");
+const resultContainerDiv = document.getElementById("result_content_container");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  genreSelectTag.innerHTML = "";
+
+  const response = await fetch(`/genres`);
+  const data = await response.json();
+  data.forEach((one) => makeSelectItem(one.Name));
+
+  genreSearchButton.addEventListener("click", () => {
+    const genreSelectValue = genreSelectTag.value;
+    searchTracks(genreSelectValue);
+  });
 });
 
+function makeSelectItem(genrename) {
+  const genreOptionTag = document.createElement("option");
+  genreOptionTag.value = genrename;
+  genreOptionTag.textContent = genrename;
+
+  genreSelectTag.appendChild(genreOptionTag);
+}
+
+async function searchTracks(genrename) {
+  resultContainerDiv.innerHTML = "";
+
+  const response = await fetch(`/genres/${genrename}`);
+  const data = await response.json();
+  console.log(data);
+  data.forEach((one) => makeListItem(one.genrename, one.trackname));
+}
+
+function makeListItem(genrename, trackname) {
+  const resultContentDiv = document.createElement("div");
+  resultContentDiv.classList.add("result_content");
+  resultContentDiv.innerHTML = `<div class="genre_name">${genrename}</div>
+    <div class="genre_track">${trackname}</div>
+    `;
+
+  resultContainerDiv.appendChild(resultContentDiv);
+}
--- 4.DATABASE/2.CHINOOK/server.js
+++ 4.DATABASE/2.CHINOOK/server.js
@@ -1,10 +1,34 @@
-const express = require('express')
-const Database = require('./database');
+const express = require("express");
+const Database = require("./database");
 
 const app = express();
 const PORT = 3000;
 const db = new Database();
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.get("/genres", (req, res) => {
+  const query = `SELECT * FROM genres`;
+  const genres = db.executeQuery(query);
+  res.send(genres);
+});
+
+app.get("/genres/:genrename", (req, res) => {
+  const genre = req.params.genrename;
+  const query = `
+SELECT g.name AS genrename, t.name AS trackname
+FROM genres g
+JOIN tracks t
+ON g.genreid = t.genreid
+Where g.name LIKE ?
+    `;
+
+    const tracks = db.executeQuery(query,[`%${genre}%`]);
+    res.send(tracks); 
+});
+
+app.listen(PORT, () => {
+  console.log("서버 레디");
+});
