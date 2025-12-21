04eecf467ebb97f599c5ed75d1d34cb580f40dce
namgijeong <dreaming1817@gmail.com>
2025-12-21 21:48:48 +0900

    [과제] chinook 페이지네이션 구현

--- 4.DATABASE/2.CHINOOK/public/script.js
+++ 4.DATABASE/2.CHINOOK/public/script.js
@@ -1,6 +1,11 @@
 const genreSelectTag = document.getElementById("genre_select");
 const genreSearchButton = document.getElementById("genre_search");
 const resultContainerDiv = document.getElementById("result_content_container");
+const resultButtonContainer = document.getElementById('result_button_container');
+const LIMIT = 10;
+const LIMITPAGE = 5;
+let genreSelectValue = '';
+
 
 document.addEventListener("DOMContentLoaded", async () => {
   genreSelectTag.innerHTML = "";
@@ -10,8 +15,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   data.forEach((one) => makeSelectItem(one.Name));
 
   genreSearchButton.addEventListener("click", () => {
-    const genreSelectValue = genreSelectTag.value;
-    searchTracks(genreSelectValue);
+    genreSelectValue = genreSelectTag.value;
+    searchTracks(genreSelectValue, 1);
   });
 });
 
@@ -23,13 +28,15 @@ function makeSelectItem(genrename) {
   genreSelectTag.appendChild(genreOptionTag);
 }
 
-async function searchTracks(genrename) {
+async function searchTracks(genrename, page) {
   resultContainerDiv.innerHTML = "";
 
-  const response = await fetch(`/genres/${genrename}`);
+  const response = await fetch(`/genres/${genrename}/${page}`);
   const data = await response.json();
   console.log(data);
-  data.forEach((one) => makeListItem(one.genrename, one.trackname));
+  data.tracks.forEach((one) => makeListItem(one.genrename, one.trackname));
+
+  makePageButton(page, data.totalcount);
 }
 
 function makeListItem(genrename, trackname) {
@@ -41,3 +48,58 @@ function makeListItem(genrename, trackname) {
 
   resultContainerDiv.appendChild(resultContentDiv);
 }
+
+function makePageButton(page, totalCount){
+  resultButtonContainer.innerHTML = '';
+
+  const totalPage = Math.ceil(totalCount / LIMIT);
+  const sectionNumber = Math.ceil(page / LIMITPAGE);
+  console.log(sectionNumber);
+  let sectionEndNumber = sectionNumber * LIMITPAGE;
+  let sectionStartNumber = sectionEndNumber - LIMITPAGE + 1;
+
+  if (sectionStartNumber < 1){
+    sectionStartNumber = 1;
+  }
+  if (sectionEndNumber > totalPage){
+    sectionEndNumber = totalPage;
+  }
+
+  //이전 버튼 만들기
+  if (sectionStartNumber > 1){
+    const prevButtonNumber = sectionStartNumber - 1;
+   
+    const prevButton = document.createElement('button');
+    prevButton.classList.add('page_button');
+    prevButton.textContent = '<';
+    prevButton.dataset.number = prevButtonNumber;
+    resultButtonContainer.appendChild(prevButton);
+  }
+
+  //숫자 버튼 만들기
+  for (let i = sectionStartNumber; i <= sectionEndNumber; i++){
+    const Button = document.createElement('button');
+    Button.classList.add('page_button');
+    Button.textContent = i;
+    Button.dataset.number = i;
+    resultButtonContainer.appendChild(Button);
+  }
+
+  //다음 버튼 만들기
+  if (sectionEndNumber < totalPage){
+    const nextButtonNumber = sectionEndNumber + 1;
+    
+    const nextButton = document.createElement('button');
+    nextButton.classList.add('page_button');
+    nextButton.textContent = '>';
+    nextButton.dataset.number = nextButtonNumber
+    resultButtonContainer.appendChild(nextButton);
+  }
+}
+
+//이벤트 위임
+resultButtonContainer.addEventListener('click', (ev) => {
+  if (ev.target.classList.contains('page_button')){
+    searchTracks(genreSelectValue, ev.target.dataset.number);
+  }
+});
--- 4.DATABASE/2.CHINOOK/server.js
+++ 4.DATABASE/2.CHINOOK/server.js
@@ -4,6 +4,7 @@ const Database = require("./database");
 const app = express();
 const PORT = 3000;
 const db = new Database();
+const LIMIT = 10;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -15,18 +16,32 @@ app.get("/genres", (req, res) => {
   res.send(genres);
 });
 
-app.get("/genres/:genrename", (req, res) => {
+app.get("/genres/:genrename/:page", (req, res) => {
   const genre = req.params.genrename;
-  const query = `
-SELECT g.name AS genrename, t.name AS trackname
-FROM genres g
-JOIN tracks t
-ON g.genreid = t.genreid
-Where g.name LIKE ?
-    `;
+  const page = Number(req.params.page);
 
-    const tracks = db.executeQuery(query,[`%${genre}%`]);
-    res.send(tracks); 
+  const offset = LIMIT * (page-1);
+  const genresquery = `
+    SELECT g.name AS genrename, t.name AS trackname
+    FROM genres g
+    JOIN tracks t
+    ON g.genreid = t.genreid
+    Where g.name LIKE ?
+    LIMIT ?
+    OFFSET ?
+  `;
+  const tracks = db.executeQuery(genresquery,[`%${genre}%`, LIMIT, offset ]);
+
+  const totalCountQuery = `
+    SELECT count(*) AS totalcount
+    FROM genres g
+    JOIN tracks t
+    ON g.genreid = t.genreid
+    Where g.name LIKE ?
+  `;
+  const totalCount = db.executeQuery(totalCountQuery,[`%${genre}%`]);
+
+  res.json({tracks:tracks, totalcount:totalCount[0].totalcount}); 
 });
 
 app.listen(PORT, () => {
