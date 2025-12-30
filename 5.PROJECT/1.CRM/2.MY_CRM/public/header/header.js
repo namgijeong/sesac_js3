3768a8addbff943f784f6fb2f9ea05d903b44cca
namgijeong <dreaming1817@gmail.com>
2025-12-30 23:04:25 +0900

    [과제] CRM PROJECT  header 네비게이션바 구현

---
+++ 5.PROJECT/1.CRM/2.MY_CRM/public/header/header.js
@@ -0,0 +1,11 @@
+const headerDiv = document.getElementById('header');
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchHeaders();
+});
+
+async function fetchHeaders(){
+  const response = await fetch(`/header/header.html`);
+  const text = await response.text();
+  headerDiv.innerHTML = text;
+}
