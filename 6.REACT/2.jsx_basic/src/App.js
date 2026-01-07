8bbac76b58aa06feae8f396205cb4d6f9cb7be24
namgijeong <dreaming1817@gmail.com>
2026-01-07 12:31:52 +0900

    [학습] react 기초 -tailwind 일부 속성 정리

---
+++ 6.REACT/2.jsx_basic/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>Hello, React</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
